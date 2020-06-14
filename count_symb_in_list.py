dbb9bfa080e4421f603d13f2bb8b1a9c5aa94bab
AshykhminGit <65783869+AshykhminGit@users.noreply.github.com>
2020-06-14 15:39:14 +0300

    comment1

---
+++ count_symb_in_list.py
@@ -0,0 +1,17 @@
+string1 = " What the  awesome 365 day to kill all 500 thousands motherfucckers. kill killer Dead pool"
+print (string1)
+
+print ("Kolichestvo probelov v etoy fraze {}".format(string1.count(' ')))
+print ("Kolichestvo bukv L  v etoy fraze {}".format(string1.count('l')))
+print ('kolichestvo cifr 5 v etoy fraxe {}'.format(string1.count("5")))
+
+
+#count word
+print("kolichestvo slov kill v etoy fraze :")
+print (string1.count('kill'))
+
+
+
+
+
+
